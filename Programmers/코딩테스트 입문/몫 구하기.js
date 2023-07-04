ccfcc17972d8f6eebd654a696cd84bd303c6436b
kys0417 <92397578+kys0417@users.noreply.github.com>
2023-07-04 18:05:22 +0900

    Update 몫 구하기.js

--- Programmers/코딩테스트 입문/몫 구하기.js
+++ Programmers/코딩테스트 입문/몫 구하기.js
@@ -1,9 +1,4 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
-
-int solution(int num1, int num2) {
+function solution(num1, num2) {
     // ~~(num1/num2) == Math.floor(num1/num2)
-    int answer = ~~(num1 / num2);
-    return answer;
+    return Math.floor(num1/num2);
 }
