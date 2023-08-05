773b7bdedbde178a4a14f1e7c6692f2e623fcdd0
김윤상 <92397578+kys0417@users.noreply.github.com>
2023-08-05 21:28:27 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.js
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.js
@@ -1,10 +1,11 @@
 function solution(money) {
-    let answer = [];
-    const coffee = ~~(money / 5500);
-    while(money >= 5500) {
-        money = ~~(money % 5500);
-    }
-    answer.push(coffee);
-    answer.push(money);
-    return answer;
+    // let answer = [];
+    // const coffee = ~~(money / 5500);
+    // while(money >= 5500) {
+    //     money = ~~(money % 5500);
+    // }
+    // answer.push(coffee);
+    // answer.push(money);
+    // return answer;
+    return [~~(money/5500), ~~(money%5500)];
 }
