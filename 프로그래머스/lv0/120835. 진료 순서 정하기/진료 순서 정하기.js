5b243e97e806e8bc4cde1135e5bc2b593da7e92d
김윤상 <92397578+kys0417@users.noreply.github.com>
2023-08-07 13:42:05 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.js
+++ 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.js
@@ -1,7 +1,8 @@
 function solution(emergency) {
     let sortEmergency = [...emergency].sort((a,b) => b-a);
-    emergency.forEach((v,i) => {
-        emergency[i] = sortEmergency.indexOf(v) + 1;
-    });
-    return emergency;
+    // emergency.forEach((v,i) => {
+    //     emergency[i] = sortEmergency.indexOf(v) + 1;
+    // });
+    // return emergency;
+    return emergency.map((v) => sortEmergency.indexOf(v) + 1);
 }
