94920d001a8cb8d6c4be1ae3043c0b588dce35fa
김윤상 <92397578+kys0417@users.noreply.github.com>
2023-08-08 17:49:31 +0900

    [level 0] Title: 가위 바위 보, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
@@ -1,11 +1,13 @@
 function solution(rsp) {
-    let answer = []
-    let rspArr = [...rsp]
-    for(let i in rspArr){
-        if(rspArr[i] === "2") rspArr[i] = "0";
-        else if(rspArr[i] === "0") rspArr[i] = "5";
-        else if(rspArr[i] === "5") rspArr[i] = "2";
-        answer.push(rspArr[i]);
-    }
-    return rspArr.join("");
+    // 풀이 1
+    // let answer = []
+    // let rspArr = [...rsp]
+    // for(let i in rspArr){
+    //     if(rspArr[i] === "2") rspArr[i] = "0";
+    //     else if(rspArr[i] === "0") rspArr[i] = "5";
+    //     else if(rspArr[i] === "5") rspArr[i] = "2";
+    //     answer.push(rspArr[i]);
+    // }
+    // return rspArr.join("");
+    return [...rsp].map(v => v === "2" ? "0" : (v === "0" ? "5" : "2")).join("");
 }
