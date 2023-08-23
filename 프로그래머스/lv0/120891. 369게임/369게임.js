2991c59b95d80133d2a59c506022b5905932561b
YOONSANG KIM <92397578+kys0417@users.noreply.github.com>
2023-08-23 22:12:46 +0900

    [level 0] Title: 369게임, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.js
+++ 프로그래머스/lv0/120891. 369게임/369게임.js
@@ -1,6 +1,4 @@
 function solution(order) {
-    let answer = 0;
-    order = String(order).split('');
-    order.forEach(v => v === "3" ? answer++ : v === "6" ? answer++ : v === "9" ? answer ++ : v)
+    var answer = 0;
     return answer;
 }
